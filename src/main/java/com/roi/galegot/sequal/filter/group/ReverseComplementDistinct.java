e91d013c1a97f668d42bd9b5a7bd36ba09b8de3c
Roi Galego <roi.galegot@gmail.com>
2018-10-28 22:53:34 +0100

    Reverse Complement Distinct

---
+++ src/main/java/com/roi/galegot/sequal/filter/group/ReverseComplementDistinct.java
@@ -0,0 +1,141 @@
+package com.roi.galegot.sequal.filter.group;
+
+import java.io.Serializable;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+
+import com.roi.galegot.sequal.common.Sequence;
+import scala.Tuple2;
+
+public class ReverseComplementDistinct implements GroupFilter {
+
+	private static final long serialVersionUID = -3416801873476204891L;
+
+	@Override
+	public JavaRDD<Sequence> validate(JavaRDD<Sequence> seqs) {
+		JavaPairRDD<ReverseComplementString, Sequence> group = seqs
+				.mapToPair(new PairFunction<Sequence, ReverseComplementString, Sequence>() {
+
+					private static final long serialVersionUID = -6192877628808422227L;
+
+					@Override
+					public Tuple2<ReverseComplementString, Sequence> call(Sequence seq) {
+						return new Tuple2<ReverseComplementString, Sequence>(new ReverseComplementString(seq.getSeq()),
+								seq);
+					}
+				});
+
+		if (seqs.first().isHasQual()) {
+			return group.reduceByKey(new Function2<Sequence, Sequence, Sequence>() {
+
+				private static final long serialVersionUID = -2285412703072618941L;
+
+				@Override
+				public Sequence call(Sequence seq1, Sequence seq2) {
+					if (seq1.getQuality() >= seq2.getQuality()) {
+						return seq1;
+					}
+					return seq2;
+				}
+			}).values();
+		} else {
+			return group.reduceByKey(new Function2<Sequence, Sequence, Sequence>() {
+
+				private static final long serialVersionUID = -6498038456070131065L;
+
+				@Override
+				public Sequence call(Sequence seq1, Sequence seq2) {
+					return seq1;
+				}
+			}).values();
+		}
+	}
+
+	class ReverseComplementString implements Serializable {
+
+		private static final long serialVersionUID = -7032196466156045735L;
+		private String s, sr;
+
+		public ReverseComplementString(String s) {
+			this.s = s;
+			this.sr = new StringBuilder(this.getComplementary(s)).reverse().toString();
+		}
+
+		private String getComplementary(String s) {
+			char[] result = new char[s.length()];
+			int counter = 0;
+			for (char c : this.s.toCharArray()) {
+
+				switch (c) {
+				case 'A':
+					result[counter] = 'T';
+					break;
+				case 'G':
+					result[counter] = 'C';
+					break;
+				case 'T':
+					result[counter] = 'A';
+					break;
+				case 'C':
+					result[counter] = 'G';
+					break;
+				default:
+					result[counter] = c;
+				}
+				counter++;
+			}
+			return String.valueOf(result);
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = (prime * result) + this.getOuterType().hashCode();
+			result = (prime * result)
+					+ (((this.s == null) ? 0 : this.s.hashCode()) + ((this.sr == null) ? 0 : this.sr.hashCode()));
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (this.getClass() != obj.getClass()) {
+				return false;
+			}
+			ReverseComplementString other = (ReverseComplementString) obj;
+			if (!this.getOuterType().equals(other.getOuterType())) {
+				return false;
+			}
+			if (this.s == null) {
+				if (other.sr != null) {
+					return false;
+				}
+			} else if (!this.s.equals(other.sr)) {
+				return false;
+			}
+			if (this.sr == null) {
+				if (other.s != null) {
+					return false;
+				}
+			} else if (!this.sr.equals(other.s)) {
+				return false;
+			}
+			return true;
+		}
+
+		private ReverseComplementDistinct getOuterType() {
+			return ReverseComplementDistinct.this;
+		}
+
+	}
+
+}
--- src/test/java/com/roi/galegot/sequal/filter/group/GroupFiltersTest.java
+++ src/test/java/com/roi/galegot/sequal/filter/group/GroupFiltersTest.java
@@ -1,5 +1,84 @@
 package com.roi.galegot.sequal.filter.group;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.roi.galegot.sequal.common.Sequence;
+
 public class GroupFiltersTest {
+	private static SparkConf spc;
+	private static JavaSparkContext jsc;
+
+	private static String commLine = "+";
+
+	@BeforeClass
+	public static void setupSpark() throws IOException {
+		spc = new SparkConf().setAppName("SeQual").setMaster("local[*]");
+		jsc = new JavaSparkContext(spc);
+		jsc.setLogLevel("ERROR");
+	}
+
+	@AfterClass
+	public static void stopSpark() {
+		jsc.close();
+	}
+
+	@Test
+	public void filterReverseComplementDistinct() {
+		String seq1s1 = "@Original";
+		String seq1s2 = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		String seq1s4 = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		Sequence seq1 = new Sequence(seq1s1, seq1s2, commLine, seq1s4);
+
+		String seq2s1 = "@ComplementReverseCalidadIgual";
+		String seq2s2 = "GGGGGTGTGTTTTTCCGATTTGGGGGGGGGA";
+		String seq2s4 = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		Sequence seq2 = new Sequence(seq2s1, seq2s2, commLine, seq2s4);
+
+		String seq3s1 = "@ComplementReverseMasCalidad";
+		String seq3s2 = "GGGGGTGTGTTTTTCCGATTTGGGGGGGGGA";
+		String seq3s4 = "5?:5;<02:@977=:<0=9>@5>7>;>*3,1";
+		Sequence seq3 = new Sequence(seq3s1, seq3s2, commLine, seq3s4);
+
+		String seq1s1fa = ">Original";
+		String seq1s2fa = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		Sequence seq1fa = new Sequence(seq1s1fa, seq1s2fa);
+
+		String seq2s1fa = ">ComplementReverse";
+		String seq2s2fa = "GGGGGTGTGTTTTTCCGATTTGGGGGGGGGA";
+		Sequence seq2fa = new Sequence(seq2s1fa, seq2s2fa);
+
+		String seq3s1fa = ">ComplementReverseCopia";
+		String seq3s2fa = "GGGGGTGTGTTTTTCCGATTTGGGGGGGGGA";
+		Sequence seq3fa = new Sequence(seq3s1fa, seq3s2fa);
+
+		JavaRDD<Sequence> original = jsc.parallelize(Arrays.asList(seq1, seq2, seq3));
+		JavaRDD<Sequence> originalFA = jsc.parallelize(Arrays.asList(seq1fa, seq2fa, seq3fa));
+		JavaRDD<Sequence> filtered;
+		ArrayList<Sequence> list;
+		GroupFilter filter = new ReverseComplementDistinct();
+
+		filtered = filter.validate(original);
+		assertEquals(filtered.count(), 1);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 1);
+		assertTrue(list.contains(seq3));
+
+		filtered = filter.validate(originalFA);
+		assertEquals(filtered.count(), 1);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 1);
+	}
 
 }
