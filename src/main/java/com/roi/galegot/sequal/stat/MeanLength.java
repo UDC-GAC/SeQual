43b2c53662b65c0e797c1b1ac510c81a443e570b
Roi Galego <roi.galegot@gmail.com>
2019-01-05 13:09:09 +0100

    MeanLength

---
+++ src/main/java/com/roi/galegot/sequal/stat/MeanLength.java
@@ -0,0 +1,131 @@
+package com.roi.galegot.sequal.stat;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function2;
+
+import com.roi.galegot.sequal.common.Sequence;
+
+/**
+ * The Class MeanLength.
+ */
+public class MeanLength implements Stat {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = -5049048767496389502L;
+
+	@Override
+	public Double measure(JavaRDD<Sequence> seqs) {
+
+		BigDecimal mean;
+		DummyCount initialDummy = new DummyCount();
+		DummyCount resultDummy;
+		Function2<DummyCount, Sequence, DummyCount> addAndCount;
+		Function2<DummyCount, DummyCount, DummyCount> combine;
+
+		addAndCount = new Function2<DummyCount, Sequence, DummyCount>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public DummyCount call(DummyCount v1, Sequence v2)
+					throws Exception {
+				v1.countLength += v2.getLength();
+				v1.countNumber = v1.countNumber + Long.valueOf(1);
+
+				return v1;
+			}
+
+		};
+
+		combine = new Function2<DummyCount, DummyCount, DummyCount>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public DummyCount call(DummyCount v1, DummyCount v2)
+					throws Exception {
+				v1.countLength += v2.countLength;
+				v1.countNumber += v2.countNumber;
+
+				return v1;
+			}
+
+		};
+
+		resultDummy = seqs.aggregate(initialDummy, addAndCount, combine);
+		mean = resultDummy.getMean();
+
+		return mean.doubleValue();
+	}
+
+	/**
+	 * The Class DummyCount.
+	 */
+	public class DummyCount implements Serializable {
+
+		/** The Constant serialVersionUID. */
+		private static final long serialVersionUID = -7774902974273299201L;
+
+		/** The count length. */
+		private Long countLength = Long.valueOf(0);
+
+		/** The count number. */
+		private Long countNumber = Long.valueOf(0);
+
+		/**
+		 * Gets the count length.
+		 *
+		 * @return the count length
+		 */
+		public Long getCountLength() {
+			return this.countLength;
+		}
+
+		/**
+		 * Sets the count length.
+		 *
+		 * @param countLength the new count length
+		 */
+		public void setCountLength(Long countLength) {
+			this.countLength = countLength;
+		}
+
+		/**
+		 * Gets the count number.
+		 *
+		 * @return the count number
+		 */
+		public Long getCountNumber() {
+			return this.countNumber;
+		}
+
+		/**
+		 * Sets the count number.
+		 *
+		 * @param countNumber the new count number
+		 */
+		public void setCountNumber(Long countNumber) {
+			this.countNumber = countNumber;
+		}
+
+		/**
+		 * Gets the mean.
+		 *
+		 * @return the mean
+		 */
+		public BigDecimal getMean() {
+			if (this.countNumber <= 0) {
+				return BigDecimal.ZERO;
+			}
+
+			return BigDecimal.valueOf(this.countLength).divide(
+					BigDecimal.valueOf(this.countNumber), 2,
+					BigDecimal.ROUND_UP);
+		}
+
+	}
+
+}
--- src/test/java/com/roi/galegot/sequal/stat/stat/StatsTest.java
+++ src/test/java/com/roi/galegot/sequal/stat/stat/StatsTest.java
@@ -1,12 +1,21 @@
 package com.roi.galegot.sequal.stat.stat;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.roi.galegot.sequal.common.Sequence;
+import com.roi.galegot.sequal.stat.MeanLength;
+import com.roi.galegot.sequal.stat.Stat;
 
 public class StatsTest {
 	private static SparkConf spc;
@@ -33,7 +42,52 @@ public class StatsTest {
 
 	@Test
 	public void statMeanLength() {
+		/*
+		 * Sequence 1 Length = 30
+		 */
+		String seq1s1 = "@cluster_8:UMI_CTTTGA";
+		String seq1s2 = "TATCCUNGCAATANTCTCCGAACNGGAGAG";
+		String seq1s4 = "1/04.72,(003,-2-22+00-12./.-.4";
+		Sequence seq1 = new Sequence(seq1s1, seq1s2, commLine, seq1s4);
 
+		/*
+		 * Sequence 2 Length = 29
+		 */
+		String seq2s1 = "@cluster_12:UMI_GGTCAA";
+		String seq2s2 = "GCAGTTNNAGATCAATATATNNNAGAGCA";
+		String seq2s4 = "?7?AEEC@>=1?A?EEEB9ECB?==:B.A";
+		Sequence seq2 = new Sequence(seq2s1, seq2s2, commLine, seq2s4);
+
+		/*
+		 * Sequence 3 Length = 28
+		 */
+		String seq3s1 = "@cluster_21:UMI_AGAACA";
+		String seq3s2 = "GGCATTGCAAAATTTNTTSCACCCCCAG";
+		String seq3s4 = ">=2.660/?:36AD;0<14703640334";
+		Sequence seq3 = new Sequence(seq3s1, seq3s2, commLine, seq3s4);
+
+		JavaRDD<Sequence> original1 = jsc.parallelize(Arrays.asList(seq1));
+		JavaRDD<Sequence> original2 = jsc
+				.parallelize(Arrays.asList(seq1, seq2));
+		JavaRDD<Sequence> original3 = jsc
+				.parallelize(Arrays.asList(seq1, seq2, seq3));
+
+		JavaRDD<Sequence> emptyRdd = jsc.parallelize(new ArrayList<Sequence>());
+		Double result;
+		Stat stat = new MeanLength();
+
+		// Test for empty RDD
+		result = stat.measure(emptyRdd);
+		assertTrue(result == 0);
+
+		result = stat.measure(original1);
+		assertTrue(result == 30);
+
+		result = stat.measure(original2);
+		assertTrue(result == 29.5);
+
+		result = stat.measure(original3);
+		assertTrue(result == 29);
 	}
 
 	@Test
