d731961cb80f744036055dc1c6f727bff887b951
Roi Galego <roi.galegot@gmail.com>
2019-02-20 09:30:21 +0100

    TrimQualLeftPair

--- src/test/java/com/roi/galegot/sequal/trim/trimmer/TrimmersTest.java
+++ src/test/java/com/roi/galegot/sequal/trim/trimmer/TrimmersTest.java
@@ -530,6 +530,151 @@ public class TrimmersTest {
 	}
 
 	@Test
+	public void trimQualLeftPair() {
+		/*
+		 * Length = 30 Quality = 14,566666
+		 */
+		String seq1s1 = "@cluster_8:UMI_CTTTGA_1";
+		String seq1s2 = "TATCCUNGCAATANTCTCCGAACNGGAGAG";
+		String seq1s4 = "1/04.72,(003,-2-22+00-12./.-.4";
+		Sequence seq1 = new Sequence(seq1s1, seq1s2, commLine, seq1s4);
+
+		String seq1s1Pair = "@cluster_8:UMI_CTTTGA_2";
+		String seq1s2Pair = "TATCCUNGCAATANTCTCCGAACNGGAGAG";
+		String seq1s4Pair = "1/04.72,(003,-2-22+00-12./.-.4";
+		seq1.setPairSequence(seq1s1Pair, seq1s2Pair, commLine, seq1s4Pair);
+
+		String seq1s1fa = ">cluster_8:UMI_CTTTGA_1";
+		Sequence seq1fa = new Sequence(seq1s1fa, seq1s2);
+
+		String seq1s1faPair = ">cluster_8:UMI_CTTTGA_2";
+		seq1fa.setPairSequence(seq1s1faPair, seq1s2Pair);
+
+		// Copy of above sequence with 1 character
+		String seq7s2 = "G";
+		String seq7s4 = "4";
+		Sequence seq7 = new Sequence(seq1s1, seq7s2, commLine, seq7s4);
+
+		String seq7s2Pair = "G";
+		String seq7s4Pair = "4";
+		seq7.setPairSequence(seq1s1Pair, seq7s2Pair, commLine, seq7s4Pair);
+
+		/*
+		 * Length = 29 Quality = 30.103448275862068
+		 */
+		String seq2s1 = "@cluster_12:UMI_GGTCAA_1";
+		String seq2s2 = "GCAGTTNNAGATCAATATATNNNAGAGCA";
+		String seq2s4 = "?7?AEEC@>=1?A?EEEB9ECB?==:B.A";
+		Sequence seq2 = new Sequence(seq2s1, seq2s2, commLine, seq2s4);
+
+		String seq2s1Pair = "@cluster_12:UMI_GGTCAA_2";
+		String seq2s2Pair = "GCAGTTNNAGATCAATATATNNNAGAGCA";
+		String seq2s4Pair = "?7?AEEC@>=1?A?EEEB9ECB?==:B.A";
+		seq2.setPairSequence(seq2s1Pair, seq2s2Pair, commLine, seq2s4Pair);
+
+		String seq2s1fa = ">cluster_12:UMI_GGTCAA_1";
+		Sequence seq2fa = new Sequence(seq2s1fa, seq2s2);
+
+		String seq2s1faPair = ">cluster_12:UMI_GGTCAA_2";
+		seq2fa.setPairSequence(seq2s1faPair, seq2s2Pair);
+
+		// Copy of above sequence with 1 character and quality = 32
+		String seq8s2 = "A";
+		String seq8s4 = "A";
+		Sequence seq8 = new Sequence(seq2s1, seq8s2, commLine, seq8s4);
+
+		String seq8s2Pair = "A";
+		String seq8s4Pair = "A";
+		seq8.setPairSequence(seq2s1Pair, seq8s2Pair, commLine, seq8s4Pair);
+
+		/*
+		 * Length = 24 Quality = 21,541666666
+		 */
+		String seq3s1 = "@cluster_21:UMI_AGAACA_1";
+		String seq3s2 = "GGCATTGCAAAATTTNTTSCACCC";
+		String seq3s4 = ">=2.660/?:36AD;0<1470364";
+		Sequence seq3 = new Sequence(seq3s1, seq3s2, commLine, seq3s4);
+
+		String seq3s1Pair = "@cluster_21:UMI_AGAACA_2";
+		String seq3s2Pair = "GGCATTGCAAAATTTNTTSCACCC";
+		String seq3s4Pair = ">=2.660/?:36AD;0<1470364";
+		seq3.setPairSequence(seq3s1Pair, seq3s2Pair, commLine, seq3s4Pair);
+
+		String seq3s1fa = ">cluster_21:UMI_AGAACA_1";
+		Sequence seq3fa = new Sequence(seq3s1fa, seq3s2);
+
+		String seq3s1faPair = ">cluster_21:UMI_AGAACA_2";
+		seq3fa.setPairSequence(seq3s1faPair, seq3s2Pair);
+
+		// Copy of above sequence with 25 characters and quality = 19,8
+		String seq5s2 = "TNTTSCACCC";
+		String seq5s4 = ";0<1470364";
+		Sequence seq5 = new Sequence(seq3s1, seq5s2, commLine, seq5s4);
+
+		String seq5s2Pair = "TNTTSCACCC";
+		String seq5s4Pair = ";0<1470364";
+		seq5.setPairSequence(seq3s1Pair, seq5s2Pair, commLine, seq5s4Pair);
+
+		// Copy of above sequence with 1 character
+		String seq9s2 = "C";
+		String seq9s4 = "4";
+		Sequence seq9 = new Sequence(seq3s1, seq9s2, commLine, seq9s4);
+
+		String seq9s2Pair = "C";
+		String seq9s4Pair = "4";
+		seq9.setPairSequence(seq3s1Pair, seq9s2Pair, commLine, seq9s4Pair);
+
+		JavaRDD<Sequence> original = jsc.parallelize(Arrays.asList(seq1, seq2, seq3));
+		JavaRDD<Sequence> originalFA = jsc.parallelize(Arrays.asList(seq1fa, seq2fa, seq3fa));
+		JavaRDD<Sequence> emptyRdd = jsc.parallelize(new ArrayList<>());
+		JavaRDD<Sequence> trimmered;
+		ArrayList<Sequence> list;
+		Trimmer trimmer = new TrimQualLeft();
+
+		// Test for empty RDD
+		trimmered = trimmer.trim(emptyRdd);
+		assertEquals(0, trimmered.count());
+
+		ExecutionParametersManager.setParameter("TrimQualLeft", "");
+		trimmered = trimmer.trim(original);
+		assertEquals(original.collect(), trimmered.collect());
+
+		ExecutionParametersManager.setParameter("TrimQualLeft", "0");
+		trimmered = trimmer.trim(original);
+		assertEquals(original.collect(), trimmered.collect());
+
+		ExecutionParametersManager.setParameter("TrimQualLeft", "-1");
+		trimmered = trimmer.trim(original);
+		assertEquals(original.collect(), trimmered.collect());
+
+		ExecutionParametersManager.setParameter("TrimQualLeft", "1");
+		trimmered = trimmer.trim(original);
+		assertEquals(3, trimmered.count());
+		list = new ArrayList<>(trimmered.collect());
+		assertEquals(3, list.size());
+		assertTrue(list.contains(seq7));
+		assertTrue(list.contains(seq8));
+		assertTrue(list.contains(seq9));
+
+		ExecutionParametersManager.setParameter("TrimQualLeft", "20");
+		trimmered = trimmer.trim(original);
+		assertEquals(3, trimmered.count());
+		list = new ArrayList<>(trimmered.collect());
+		assertEquals(3, list.size());
+		assertTrue(list.contains(seq1));
+		assertTrue(list.contains(seq5));
+		assertTrue(list.contains(seq8));
+
+		trimmered = trimmer.trim(originalFA);
+		assertEquals(3, trimmered.count());
+		list = new ArrayList<>(trimmered.collect());
+		assertEquals(3, list.size());
+		assertTrue(list.contains(seq1fa));
+		assertTrue(list.contains(seq2fa));
+		assertTrue(list.contains(seq3fa));
+	}
+
+	@Test
 	public void trimQualRight() {
 		/*
 		 * Length = 30 Quality = 14,566666
