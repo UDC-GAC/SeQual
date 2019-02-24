aa72964d6e1e3b5ff08f5e0449bd6d141bc1cf7c
Roi Galego <roi.galegot@gmail.com>
2019-02-24 11:15:28 +0100

    MeanQualityPair

--- src/main/java/com/roi/galegot/sequal/stat/MeanQuality.java
+++ src/main/java/com/roi/galegot/sequal/stat/MeanQuality.java
@@ -13,19 +13,26 @@ import com.roi.galegot.sequal.common.Sequence;
  */
 public class MeanQuality implements Stat {
 
-	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 8125961407288475771L;
 
+	/**
+	 * Measure.
+	 *
+	 * @param sequences the sequences
+	 * @return the double
+	 */
 	@Override
-	public Double measure(JavaRDD<Sequence> seqs) {
-
-		// TODO Check how to adapt it to Paired End Sequences
+	public Double measure(JavaRDD<Sequence> sequences) {
 
 		BigDecimal mean;
 		DummyCount initialDummy = new DummyCount();
 		DummyCount resultDummy;
 		Function2<DummyCount, Sequence, DummyCount> addAndCount;
 		Function2<DummyCount, DummyCount, DummyCount> combine;
+
+		if (sequences.isEmpty() || !sequences.first().getHasQuality()) {
+			return Double.valueOf(0);
+		}
 
 		addAndCount = new Function2<DummyCount, Sequence, DummyCount>() {
 
@@ -46,17 +53,73 @@ public class MeanQuality implements Stat {
 
 			@Override
 			public DummyCount call(DummyCount v1, DummyCount v2) throws Exception {
-				v1.countQuality = v1.countQuality.add(v2.countQuality);
-				v1.countNumber += v2.countNumber;
+				return MeanQuality.this.combineFunction(v1, v2);
+			}
+
+		};
+
+		resultDummy = sequences.aggregate(initialDummy, addAndCount, combine);
+		mean = resultDummy.getMean();
+
+		return mean.doubleValue();
+	}
+
+	/**
+	 * Measure pair.
+	 *
+	 * @param sequences the sequences
+	 * @return the double
+	 */
+	@Override
+	public Double measurePair(JavaRDD<Sequence> sequences) {
+		BigDecimal mean;
+		DummyCount initialDummy = new DummyCount();
+		DummyCount resultDummy;
+		Function2<DummyCount, Sequence, DummyCount> addAndCount;
+		Function2<DummyCount, DummyCount, DummyCount> combine;
+
+		addAndCount = new Function2<DummyCount, Sequence, DummyCount>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public DummyCount call(DummyCount v1, Sequence v2) throws Exception {
+				v1.countQuality = v1.countQuality.add(BigDecimal.valueOf(v2.getQualityPair()));
+				v1.countNumber += (long) 1;
 				return v1;
 			}
 
 		};
 
-		resultDummy = seqs.aggregate(initialDummy, addAndCount, combine);
+		combine = new Function2<DummyCount, DummyCount, DummyCount>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public DummyCount call(DummyCount v1, DummyCount v2) throws Exception {
+				return MeanQuality.this.combineFunction(v1, v2);
+			}
+
+		};
+
+		resultDummy = sequences.aggregate(initialDummy, addAndCount, combine);
 		mean = resultDummy.getMean();
 
 		return mean.doubleValue();
+	}
+
+	/**
+	 * Combine function.
+	 *
+	 * @param v1 the v 1
+	 * @param v2 the v 2
+	 * @return the dummy count
+	 * @throws Exception the exception
+	 */
+	private DummyCount combineFunction(DummyCount v1, DummyCount v2) throws Exception {
+		v1.countQuality = v1.countQuality.add(v2.countQuality);
+		v1.countNumber += v2.countNumber;
+		return v1;
 	}
 
 	/**
@@ -64,13 +127,8 @@ public class MeanQuality implements Stat {
 	 */
 	public class DummyCount implements Serializable {
 
-		/** The Constant serialVersionUID. */
 		private static final long serialVersionUID = -7774902974273299201L;
-
-		/** The count quality. */
 		private BigDecimal countQuality = BigDecimal.ZERO;
-
-		/** The count number. */
 		private Long countNumber = Long.valueOf(0);
 
 		/**
