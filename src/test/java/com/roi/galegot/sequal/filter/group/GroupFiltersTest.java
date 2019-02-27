fe73eb85d7b778f8450f6f7c2f963f5d3789e663
Roi Galego <roi.galegot@gmail.com>
2019-02-27 22:19:38 +0100

    AlmostDistinctPair

--- src/test/java/com/roi/galegot/sequal/filter/group/GroupFiltersTest.java
+++ src/test/java/com/roi/galegot/sequal/filter/group/GroupFiltersTest.java
@@ -131,10 +131,8 @@ public class GroupFiltersTest {
 		String seq4s2fa = "TCCCCCCCCCAAATCGGAAAAACACACCCAA";
 		Sequence seq4fa = new Sequence(seq4s1fa, seq4s2fa);
 
-		JavaRDD<Sequence> original = jsc
-				.parallelize(Arrays.asList(seq1, seq2, seq3, seq4));
-		JavaRDD<Sequence> originalFA = jsc
-				.parallelize(Arrays.asList(seq1fa, seq2fa, seq3fa, seq4fa));
+		JavaRDD<Sequence> original = jsc.parallelize(Arrays.asList(seq1, seq2, seq3, seq4));
+		JavaRDD<Sequence> originalFA = jsc.parallelize(Arrays.asList(seq1fa, seq2fa, seq3fa, seq4fa));
 		JavaRDD<Sequence> emptyRdd = jsc.parallelize(new ArrayList<Sequence>());
 		JavaRDD<Sequence> filtered;
 		ArrayList<Sequence> list;
@@ -182,8 +180,7 @@ public class GroupFiltersTest {
 		assertEquals(filtered.count(), 2);
 		list = new ArrayList<>(filtered.collect());
 		assertEquals(list.size(), 2);
-		assertTrue(list.contains(seq1fa) || list.contains(seq2fa)
-				|| list.contains(seq3fa));
+		assertTrue(list.contains(seq1fa) || list.contains(seq2fa) || list.contains(seq3fa));
 		assertTrue(list.contains(seq4fa));
 
 		ExecutionParametersManager.setParameter("MaxDifference", "2");
@@ -191,8 +188,141 @@ public class GroupFiltersTest {
 		assertEquals(filtered.count(), 1);
 		list = new ArrayList<>(filtered.collect());
 		assertEquals(list.size(), 1);
-		assertTrue(list.contains(seq1fa) || list.contains(seq2fa)
-				|| list.contains(seq3fa) || list.contains(seq4fa));
+		assertTrue(list.contains(seq1fa) || list.contains(seq2fa) || list.contains(seq3fa) || list.contains(seq4fa));
+	}
+
+	@Test
+	public void filterAlmostDistinctPair() {
+		String seq1s1 = "@Original_1";
+		String seq1s2 = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		String seq1s4 = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		Sequence seq1 = new Sequence(seq1s1, seq1s2, commLine, seq1s4);
+
+		String seq1s1Pair = "@Original_2";
+		String seq1s2Pair = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		String seq1s4Pair = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		seq1.setPairSequence(seq1s1Pair, seq1s2Pair, commLine, seq1s4Pair);
+
+		String seq2s1 = "@OriginalCopiaMasCalidad_1";
+		String seq2s2 = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		String seq2s4 = "5?:5;<02:@977=:<0=9>@5>7>;>*3,1";
+		Sequence seq2 = new Sequence(seq2s1, seq2s2, commLine, seq2s4);
+
+		String seq2s1Pair = "@OriginalCopiaMasCalidad_2";
+		String seq2s2Pair = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		String seq2s4Pair = "5?:5;<02:@977=:<0=9>@5>7>;>*3,1";
+		seq2.setPairSequence(seq2s1Pair, seq2s2Pair, commLine, seq2s4Pair);
+
+		String seq3s1 = "@OriginalUnoDistinto_1";
+		String seq3s2 = "TCCCCCCCCCAAATCGGAAAAACACACCCCA";
+		String seq3s4 = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		Sequence seq3 = new Sequence(seq3s1, seq3s2, commLine, seq3s4);
+
+		String seq3s1Pair = "@OriginalUnoDistinto_2";
+		String seq3s2Pair = "TCCCCCCCCCAAATCGGAAAAACACACCCCA";
+		String seq3s4Pair = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		seq3.setPairSequence(seq3s1Pair, seq3s2Pair, commLine, seq3s4Pair);
+
+		String seq4s1 = "@OriginalDosDistintos_1";
+		String seq4s2 = "TCCCCCCCCCAAATCGGAAAAACACACCCAA";
+		String seq4s4 = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		Sequence seq4 = new Sequence(seq4s1, seq4s2, commLine, seq4s4);
+
+		String seq4s1Pair = "@OriginalDosDistintos_2";
+		String seq4s2Pair = "TCCCCCCCCCAAATCGGAAAAACACACCCAA";
+		String seq4s4Pair = "5?:5;<02:@977=:<0=9>@5>7>;>*3,-";
+		seq4.setPairSequence(seq4s1Pair, seq4s2Pair, commLine, seq4s4Pair);
+
+		String seq1s1fa = ">Original_1";
+		String seq1s2fa = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		Sequence seq1fa = new Sequence(seq1s1fa, seq1s2fa);
+
+		String seq1s1faPair = ">Original_2";
+		String seq1s2faPair = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		seq1fa.setPairSequence(seq1s1faPair, seq1s2faPair);
+
+		String seq2s1fa = ">OriginalCopia_1";
+		String seq2s2fa = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		Sequence seq2fa = new Sequence(seq2s1fa, seq2s2fa);
+
+		String seq2s1faPair = ">OriginalCopia_2";
+		String seq2s2faPair = "TCCCCCCCCCAAATCGGAAAAACACACCCCC";
+		seq2fa.setPairSequence(seq2s1faPair, seq2s2faPair);
+
+		String seq3s1fa = ">OriginalUnoDistinto_1";
+		String seq3s2fa = "TCCCCCCCCCAAATCGGAAAAACACACCCCA";
+		Sequence seq3fa = new Sequence(seq3s1fa, seq3s2fa);
+
+		String seq3s1faPair = ">OriginalUnoDistinto_2";
+		String seq3s2faPair = "TCCCCCCCCCAAATCGGAAAAACACACCCCA";
+		seq3fa.setPairSequence(seq3s1faPair, seq3s2faPair);
+
+		String seq4s1fa = ">OriginalDosDistintos_1";
+		String seq4s2fa = "TCCCCCCCCCAAATCGGAAAAACACACCCAA";
+		Sequence seq4fa = new Sequence(seq4s1fa, seq4s2fa);
+
+		String seq4s1faPair = ">OriginalDosDistintos_2";
+		String seq4s2faPair = "TCCCCCCCCCAAATCGGAAAAACACACCCAA";
+		seq4fa.setPairSequence(seq4s1faPair, seq4s2faPair);
+
+		JavaRDD<Sequence> original = jsc.parallelize(Arrays.asList(seq1, seq2, seq3, seq4));
+		JavaRDD<Sequence> originalFA = jsc.parallelize(Arrays.asList(seq1fa, seq2fa, seq3fa, seq4fa));
+		JavaRDD<Sequence> emptyRdd = jsc.parallelize(new ArrayList<Sequence>());
+		JavaRDD<Sequence> filtered;
+		ArrayList<Sequence> list;
+		GroupFilter filter = new AlmostDistinct();
+
+		// Test for empty RDD
+		filtered = filter.validate(emptyRdd);
+		assertEquals(filtered.count(), 0);
+
+		ExecutionParametersManager.setParameter("MaxDifference", "");
+		filtered = filter.validate(original);
+		assertEquals(filtered.count(), 3);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 3);
+		assertTrue(list.contains(seq2));
+		assertTrue(list.contains(seq3));
+		assertTrue(list.contains(seq4));
+
+		ExecutionParametersManager.setParameter("MaxDifference", "1");
+		filtered = filter.validate(original);
+		assertEquals(filtered.count(), 2);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 2);
+		assertTrue(list.contains(seq2));
+		assertTrue(list.contains(seq4));
+
+		ExecutionParametersManager.setParameter("MaxDifference", "2");
+		filtered = filter.validate(original);
+		assertEquals(filtered.count(), 1);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 1);
+		assertTrue(list.contains(seq2));
+
+		ExecutionParametersManager.setParameter("MaxDifference", "");
+		filtered = filter.validate(originalFA);
+		assertEquals(filtered.count(), 3);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 3);
+		assertTrue(list.contains(seq1fa) || list.contains(seq2fa));
+		assertTrue(list.contains(seq3fa));
+		assertTrue(list.contains(seq4fa));
+
+		ExecutionParametersManager.setParameter("MaxDifference", "1");
+		filtered = filter.validate(originalFA);
+		assertEquals(filtered.count(), 2);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 2);
+		assertTrue(list.contains(seq1fa) || list.contains(seq2fa) || list.contains(seq3fa));
+		assertTrue(list.contains(seq4fa));
+
+		ExecutionParametersManager.setParameter("MaxDifference", "2");
+		filtered = filter.validate(originalFA);
+		assertEquals(filtered.count(), 1);
+		list = new ArrayList<>(filtered.collect());
+		assertEquals(list.size(), 1);
+		assertTrue(list.contains(seq1fa) || list.contains(seq2fa) || list.contains(seq3fa) || list.contains(seq4fa));
 	}
 
 	@Test
